75e3b84bc0e8add46b4e11119b225daa34df082b
knome7337 <pranavlearning@gmail.com>
2026-07-18 15:30:15 +0530

    v1: workout session viewer with exercise cards and rest timer

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'workout-v1';
+const URLS = [
+  '.',
+  'workout.html',
+  'img/bench-press.png',
+  'img/seated-row.png',
+  'img/shoulder-press.png',
+  'img/plank.png',
+  'img/dead-bug.png',
+  'img/squat.png',
+  'img/deadlift.png',
+  'img/glute-bridge.png',
+  'img/thoracic-rotation.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
